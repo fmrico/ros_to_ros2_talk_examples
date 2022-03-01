0d2e773836d61521b0a855dfc161c107c268e498
Francisco Martín Rico <fmrico@gmail.com>
2022-03-01 19:22:38 +0100

    Update
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- intro_ros2/src/simple_node.cpp
+++ intro_ros2/src/simple_node.cpp
@@ -12,13 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <chrono>
+
 #include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
   auto node = rclcpp::Node::make_shared("simple_node");
+  node->declare_parameter("rate");
+
+  std::chrono::nanoseconds rate_ = 200ms;
+
+  double rate = 1.0 / std::chrono::duration<double>(rate_).count();
+ 
+  std::cerr << "default rate = " << rate << std::endl;
+ 
+  node->get_parameter_or("rate", rate, rate);
+ 
+  std::cerr << 1.0 / rate << std::endl;
+
+  std::chrono::nanoseconds rate2_ =  std::chrono::duration_cast<std::chrono::steady_clock::duration>(std::chrono::duration<double>(1.0 / rate));
+  
+  // std::chrono::duration<double, std::ratio<1>>(1.0 / rate)
+
+  if (rate_ == rate2_) {
+    std::cerr << "Son iguales" << std::endl;
+  } else {
+    std::cerr << "Son diferentes" << std::endl;
+  }
+  //double rate2 = 1.0 / std::chrono::duration<double>(rate2_).count();
+
+
+
+  std::cerr << "final rate = " << rate << std::endl;
+
+  //rate_ = rate2_;
 
   rclcpp::spin(node);
 
